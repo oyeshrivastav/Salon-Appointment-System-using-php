6a21c142d5dfe10505a2da7e59421367a77ced5c
Ankur Kumar <ankurshrivastav1503@gmail.com>
2024-02-17 13:41:18 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<div class="card">
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <footer class="footer" style="text-align: center; color: rgb(92, 90, 90);"><p> Salon Appointment System &copy; Designed by Ankur Kumar</p></footer>
+    </blockquote>
+  </div>
+</div>
+
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php" style="font-family: 'Courier New', Courier, monospace;">Salon Appointment System</a>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">Services</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
