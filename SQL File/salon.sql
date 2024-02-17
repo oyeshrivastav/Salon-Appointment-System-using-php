fdf0eb64a67a5f9d3e3feab61e085f4f7fa46625
Ankur Kumar <113172722+oyeshrivastav@users.noreply.github.com>
2024-02-17 13:43:09 +0530

    Add files via upload

---
+++ SQL File/salon.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 28, 2023 at 02:14 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `salon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblappointment`
+--
+
+CREATE TABLE `tblappointment` (
+  `ID` int(10) NOT NULL,
+  `AptNumber` varchar(80) DEFAULT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `PhoneNumber` bigint(11) DEFAULT NULL,
+  `AptDate` varchar(120) DEFAULT NULL,
+  `AptTime` varchar(120) DEFAULT NULL,
+  `Services` varchar(120) DEFAULT NULL,
+  `ApplyDate` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(250) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `RemarkDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblservices`
+--
+
+CREATE TABLE `tblservices` (
+  `ID` int(10) NOT NULL,
+  `ServiceName` varchar(200) DEFAULT NULL,
+  `Cost` int(10) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tblservices`
+--
+
+INSERT INTO `tblservices` (`ID`, `ServiceName`, `Cost`, `CreationDate`) VALUES
+(1, 'Facial Treatments', 1200, '2019-07-25 11:22:38'),
+(2, 'Fruit Facial', 500, '2019-07-25 11:22:53'),
+(3, 'Charcol Facial', 1000, '2019-07-25 11:23:10'),
+(4, 'Manicures and Pedicures for Men', 500, '2019-07-25 11:23:34'),
+(5, 'Deluxe Pedicure', 600, '2019-07-25 11:23:47'),
+(6, 'Normal Menicure', 300, '2019-07-25 11:24:01'),
+(7, 'Normal Pedicure', 400, '2019-07-25 11:24:19'),
+(8, 'U-Shape Hair Cut', 250, '2019-07-25 11:24:38'),
+(9, 'Loreal Hair Color(Full)', 1200, '2019-07-25 11:25:35'),
+(10, 'Body Spa', 1500, '2019-08-19 13:36:27'),
+(11, 'Skincare Products and Regimens for Men', 1000, '2023-12-28 10:07:32');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblappointment`
+--
+ALTER TABLE `tblappointment`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblservices`
+--
+ALTER TABLE `tblservices`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblappointment`
+--
+ALTER TABLE `tblappointment`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblservices`
+--
+ALTER TABLE `tblservices`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
